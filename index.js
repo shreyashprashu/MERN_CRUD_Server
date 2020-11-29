67e0a29798e93999a47cb88cfb9faf5806f4775c
shreyash prashu <shreyashprashu93@gmail.com>
2020-11-30 00:40:32 +0530

    Memories Server

--- index.js
+++ index.js
@@ -12,9 +12,9 @@ app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 app.use('/posts',postRoutes);
 
-// app.get('/',(req,res)=>{
-//     res.send('Hello to Memories API');
-// })
+app.get('/',(req,res)=>{
+    res.send('Hello to Memories API');
+})
 const PORT =process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL ,{useNewUrlParser:true,useUnifiedTopology:true})
